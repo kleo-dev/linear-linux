24194dfdc9bf54beb44da5acca5acb22fbb1b946
Leo dev <klesti.selimay@icloud.com>
2024-12-30 04:09:41 -0600

    Added curl support and updated readme

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-wget -O ~/.linear.AppImage https://github.com/kleo-dev/linear-linux/releases/download/v0.2.0/linear-linux-0.2.0.AppImage
+curl -o ~/.linear.AppImage https://github.com/kleo-dev/linear-linux/releases/download/v0.2.0/linear-linux-0.2.0.AppImage
 sudo chmod +x ~/.linear.AppImage
 sudo mv ~/.linear.AppImage /bin/linear
